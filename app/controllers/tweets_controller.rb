e0aa7cd51faee39ac6c9476c7564d296099e3f5a
Belinda Zeng <belindazeng@college.harvard.edu>
2015-03-07 14:03:28 -0500

    just added db functionality so we can create tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,21 @@ class TweetsController < ApplicationController
 		# create a new instance of tweet (which is a model)
 		@tweet = Tweet.new
 	end
+
+	# we need to define create action, so our controller knows which page to send to
+	def create
+		# entirely different tweet from the previous
+		@tweet = Tweet.new(tweet_params)
+		
+		# save tweet to the database
+		@tweet.save
+		# goes back to new
+		render 'new'
+	end
+
+	# need to sanitize input else forbidden access
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
 end
